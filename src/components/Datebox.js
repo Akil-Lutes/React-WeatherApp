01c2b3ed31d917d055878b18cffa316d35725e1f
Akil Lutes <lutes.akil@gmail.com>
2021-09-30 00:28:17 -0500

    responsive changes to header, searchbox

--- src/components/Datebox.js
+++ src/components/Datebox.js
@@ -19,7 +19,7 @@ return (
     // I can change the layout of the weather information, ex. Make the city name font bigger and bolder.
     // I can add fa icons for the wind
     // I can nest if statement if < sunrise && > sunset ........ for night mode pictures/background
-        <div className="main-container text-white text-center bg-gray-400 bg-opacity-25 rounded-md m-auto mt-20 border-2 border-yellow-600 w-1/5 p-4 px-6">
+        <div className="main-container text-white text-center bg-gray-400 bg-opacity-25 rounded-md m-auto mt-20 border-2 border-yellow-600 md:w-1/5 p-4 px-6">
            
             {(typeof weather.main != "undefined") ? (
                 <div className="city">
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className="border border-black shadow-2xl bg-gray-300 opacity-55 rounded-full py-3 px-6 flex flex-wrap content-start justify-center w-2/12 mx-auto h-28">
-            <div className="text-green-600 text-2xl flex">
+        <div className="border border-black shadow-2xl bg-gray-300 opacity-55 rounded-full py-3 px-6 flex flex-wrap content-start justify-center md:w-2/12 mx-auto md:h-28">
+            <div className="text-green-600 flex text-sm md:text-xl">
                 Weather Forecast
             </div>
-            <div className="flex text-2xl text-center">
+            <div className="flex text-center text-sm md:text-xl">
             Search for your location!
             </div>
         </div>
